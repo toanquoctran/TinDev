665fee5e2f414a86e4f70eb0099a066a5675800a
toanquoctran <toanquoctran111001@gmail.com>
2021-09-30 00:36:57 +0700

    buttons' functionalities

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import TinderCards from './TinderCards';
 import SwipeButtons from './SwipeButtons';
@@ -9,6 +9,33 @@ import ChatScreen from './ChatScreen';
 import './App.css';
 
 const App = () => {
+  const [favorite, setFavorite] = useState({
+    like: [],
+    dislike: [],
+  });
+  const [cur, setCur] = useState(0);
+  const [people, setPeople] = useState([
+    {
+      name: 'Duc Do',
+      url: 'https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/242216139_1020020072170599_2068511528324036151_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=z6WXkq1vT9kAX8rGbUZ&tn=9tzgOjqBxAu_I11P&_nc_ht=scontent.fdad1-2.fna&oh=234ba292fc11af247cd971eb4225a20a&oe=61715E83',
+    },
+    {
+      name: 'Kendall Jenner',
+      url: 'https://scontent.fdad1-1.fna.fbcdn.net/v/t1.6435-9/241768976_394444782040038_7908778398023538209_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=oiCZoSlRnsUAX_uDFM8&_nc_ht=scontent.fdad1-1.fna&oh=a966116e903639befc21572b66f191a4&oe=61706F23',
+    },
+    {
+      name: 'Hoang Ngo',
+      url: 'https://scontent.fdad1-3.fna.fbcdn.net/v/t1.6435-9/148624205_2824318111141643_5028140707721031415_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yp7VjX2k0VsAX_D6H0V&_nc_ht=scontent.fdad1-3.fna&oh=b590d22d4656c91f30d013d1e16a8946&oe=617098B3',
+    },
+    {
+      name: 'Ariana Grande',
+      url: 'https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/241668134_397583885054157_4586748835080075007_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=mzXP1wkeLQMAX8_Fvp6&_nc_ht=scontent.fdad1-2.fna&oh=329a9675817fd17955809b43273aca59&oe=61798BFB',
+    },
+    {
+      name: 'Huy Truong',
+      url: 'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.15752-9/241451763_669768343985900_783407437978965873_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=j7HuioWE5pUAX88IWoR&_nc_ht=scontent.fdad2-1.fna&oh=8d4007f30ce2a9a2f954792674832411&oe=61704046',
+    },
+  ]);
   return (
     <div className="App">
       <Router>
@@ -25,9 +52,14 @@ const App = () => {
           </Route>
 
           <Route path="/">
-            <Header />
-            <TinderCards />
-            <SwipeButtons />
+            <Header favorite={favorite} />
+            <TinderCards
+              favorite={favorite}
+              setFavorite={setFavorite}
+              setCur={setCur}
+              cur={cur}
+              people={people}
+            />
           </Route>
         </Switch>
       </Router>
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,80 +1,153 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useMemo} from 'react';
 import TinderCard from 'react-tinder-card';
 import database from './firebase';
 import './TinderCards.css';
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 
-const TinderCards = () => {
-  const [people, setPeople] = useState([
-    {
-      name: 'Duc Do',
-      url: 'https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/242216139_1020020072170599_2068511528324036151_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=z6WXkq1vT9kAX8rGbUZ&tn=9tzgOjqBxAu_I11P&_nc_ht=scontent.fdad1-2.fna&oh=234ba292fc11af247cd971eb4225a20a&oe=61715E83',
-    },
-    {
-      name: 'Kendall Jenner',
-      url: 'https://scontent.fdad1-1.fna.fbcdn.net/v/t1.6435-9/241768976_394444782040038_7908778398023538209_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=oiCZoSlRnsUAX_uDFM8&_nc_ht=scontent.fdad1-1.fna&oh=a966116e903639befc21572b66f191a4&oe=61706F23',
-    },
-    {
-      name: 'Hoang Ngo',
-      url: 'https://scontent.fdad1-3.fna.fbcdn.net/v/t1.6435-9/148624205_2824318111141643_5028140707721031415_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yp7VjX2k0VsAX_D6H0V&_nc_ht=scontent.fdad1-3.fna&oh=b590d22d4656c91f30d013d1e16a8946&oe=617098B3',
-    },
-    {
-      name: 'Ariana Grande',
-      url: 'https://scontent.fdad1-1.fna.fbcdn.net/v/t39.30808-6/241176508_1215449052236906_8794995041286147308_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=b9115d&_nc_ohc=uk5lks5ffUIAX8vyRIH&_nc_ht=scontent.fdad1-1.fna&oh=18df24dab6a68cfbbae636d232f919de&oe=6151CBA1',
-    },
-    {
-      name: 'Huy Truong',
-      url: 'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.15752-9/241451763_669768343985900_783407437978965873_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=j7HuioWE5pUAX88IWoR&_nc_ht=scontent.fdad2-1.fna&oh=8d4007f30ce2a9a2f954792674832411&oe=61704046',
-    },
-    {
-      name: 'Madison Beer',
-      url: 'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.6435-9/167130185_468273291288243_2782201234129467819_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8631f5&_nc_ohc=KGVNsiNE8_wAX8gKybq&_nc_ht=scontent.fdad2-1.fna&oh=74bf14160e632f66b778e2ec2b20c927&oe=61725705',
-    },
-  ]);
+const db = [
+  {
+    name: 'Duc Do',
+    url: 'https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/242216139_1020020072170599_2068511528324036151_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=z6WXkq1vT9kAX8rGbUZ&tn=9tzgOjqBxAu_I11P&_nc_ht=scontent.fdad1-2.fna&oh=234ba292fc11af247cd971eb4225a20a&oe=61715E83',
+  },
+  {
+    name: 'Kendall Jenner',
+    url: 'https://scontent.fdad1-1.fna.fbcdn.net/v/t1.6435-9/241768976_394444782040038_7908778398023538209_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=oiCZoSlRnsUAX_uDFM8&_nc_ht=scontent.fdad1-1.fna&oh=a966116e903639befc21572b66f191a4&oe=61706F23',
+  },
+  {
+    name: 'Hoang Ngo',
+    url: 'https://scontent.fdad1-3.fna.fbcdn.net/v/t1.6435-9/148624205_2824318111141643_5028140707721031415_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=yp7VjX2k0VsAX_D6H0V&_nc_ht=scontent.fdad1-3.fna&oh=b590d22d4656c91f30d013d1e16a8946&oe=617098B3',
+  },
+  {
+    name: 'Ariana Grande',
+    url: 'https://scontent.fdad1-2.fna.fbcdn.net/v/t1.6435-9/241668134_397583885054157_4586748835080075007_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=mzXP1wkeLQMAX_fhgn9&_nc_ht=scontent.fdad1-2.fna&oh=dd5d45af722aa5876278b139b16acdc6&oe=61798BFB',
+  },
+  {
+    name: 'Huy Truong',
+    url: 'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.15752-9/241451763_669768343985900_783407437978965873_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=j7HuioWE5pUAX88IWoR&_nc_ht=scontent.fdad2-1.fna&oh=8d4007f30ce2a9a2f954792674832411&oe=61704046',
+  },
+  {
+    name: 'Madison Beer',
+    url: 'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.6435-9/167130185_468273291288243_2782201234129467819_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8631f5&_nc_ohc=KGVNsiNE8_wAX8gKybq&_nc_ht=scontent.fdad2-1.fna&oh=74bf14160e632f66b778e2ec2b20c927&oe=61725705',
+  },
+];
 
-  //   Piece of code which runs based on a condition
-  useEffect(() => {
-    //   this is where the code runs
+const alreadyRemoved = [];
+let charactersState = db; // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
 
-    const unsubcribe = database
-      .collection('people')
-      .onSnapshot((snapshot) =>
-        setPeople(snapshot.docs.map((doc) => doc.data()))
-      );
+function TinderCards({favorite, setFavorite, cur, setCur, people}) {
+  const [characters, setCharacters] = useState(db);
+  const [lastDirection, setLastDirection] = useState();
 
-    return () => {
-      // this is the cleanup
-      unsubcribe();
-    };
-    // this will run ONCE when the components load and never again
-  }, []);
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
 
-  // const people = [];
-  // people.push("Duc", "Hoang", "Huy")
-  // INSTEAD, use the below line
-  // setPeople([...people, "Duc", "Hoang", "Huy"])
+  const swiped = (direction, nameToDelete) => {
+    setLastDirection(direction);
+    alreadyRemoved.push(nameToDelete);
+    console.log('swiped');
+    if (direction === 'left') {
+      let newD = favorite.dislike;
+      newD.push(nameToDelete);
+      setFavorite({
+        like: favorite.like,
+        dislike: newD,
+      });
+    } else if (direction === 'right') {
+      let newL = favorite.like;
+      newL.push(nameToDelete);
+      setFavorite({
+        like: newL,
+        dislike: favorite.dislike,
+      });
+    }
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!');
+    charactersState = charactersState.filter(
+      (character) => character.name !== name
+    );
+    setCharacters(charactersState);
+  };
+
+  const swipe = (dir) => {
+    console.log('swipe');
+    const cardsLeft = characters.filter(
+      (person) => !alreadyRemoved.includes(person.name)
+    );
+    if (cardsLeft.length) {
+      const toBeRemoved = cardsLeft[cardsLeft.length - 1].name; // Find the card object to be removed
+      const index = db.map((person) => person.name).indexOf(toBeRemoved); // Find the index of which to make the reference to
+      alreadyRemoved.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
+      childRefs[index].current.swipe(dir); // Swipe the card!
+    }
+  };
 
   return (
     <div>
-      {/* <h1>Tinder cards</h1> */}
-
-      <div className="tinderCards__cardContainer">
-        {people.map((person) => (
-          <TinderCard
-            className="swipe"
-            key={person.name}
-            preventSwipe={['up, down']}
-          >
-            <div
-              style={{backgroundImage: `url(${person.url})`}}
-              className="card"
+      <div>
+        <link
+          href="https://fonts.googleapis.com/css?family=Damion&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
+          rel="stylesheet"
+        />
+        <div className="tinderCards__cardContainer">
+          {characters.map((character, index) => (
+            <TinderCard
+              ref={childRefs[index]}
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
             >
-              <h3>{person.name}</h3>
-            </div>
-          </TinderCard>
-        ))}
+              <div
+                style={{backgroundImage: 'url(' + character.url + ')'}}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+      </div>
+      <div className="swipeButtons">
+        <IconButton className="swipeButtons__repeat">
+          <ReplayIcon fontSize="large" />
+        </IconButton>
+        <IconButton
+          className="swipeButtons__left"
+          onClick={() => swipe('left')}
+        >
+          <CloseIcon fontSize="large" />
+        </IconButton>
+        <IconButton className="swipeButtons__star">
+          <StarRateIcon fontSize="large" />
+        </IconButton>
+        <IconButton
+          className="swipeButtons__right"
+          onClick={() => swipe('right')}
+        >
+          <FavoriteIcon fontSize="large" />
+        </IconButton>
+        <IconButton className="swipeButtons__flash">
+          <FlashOnIcon fontSize="large" />
+        </IconButton>
       </div>
     </div>
   );
-};
+}
 
 export default TinderCards;
